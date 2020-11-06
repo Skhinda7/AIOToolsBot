c6ae2c9cf558f8b81f4ebd118fe473031430aadc
Skhinda7 <73970774+Skhinda7@users.noreply.github.com>
2020-11-05 23:23:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-const token = 'Nzc0MDQ3ODA5MDU3ODQ5MzQ2.X6SGMg.a42buXwS12LVVsB5sKO6WefJhrc';
+const token = 'YOURTOKENHERE';
 
 const PREFIX = '$';
 
 bot.on('message', (msg) => {
-   if (msg.content === '!test'){
+   if (msg.content === '$test'){
     msg.reply(`Hello ${msg.author}!`);
    }
+}); 
+// This command was created as a test to make sure the bot works correctly and doesn't send the same message multiple times.
+bot.on('message', (msg) => {
+    if (msg.content === '$downloads'){
+        msg.reply(`Okay here you go!
+        **ANB AIO** (http://bit.ly/ANB-AIO-Setup)
+        **ANB AIO V2** (http://downloadsv2.aiobot.com/)
+        **BNB** (http://bnba.io/download-bnb)
+        **Balko (Mac - Zip)** (http://rb.gy/6ya323)
+        **Balko (Mac)** (http://rb.gy/zagmsj)
+        **Balko (Windows)** (https://rb.gy/stcykc)
+        **Balko Cookie Extension** (https://s3.amazonaws.com/balkobot.com/BalkoExtension.zip)
+        **CyberAIO** (https://rb.gy/4q2cck)
+        **Dashe** (http://rb.gy/iceabf)
+        **EveAIO** (https://rb.gy/lmzgya)\n
+        **EveCaptcha (Mac)** (https://rb.gy/4mnhky)
+        **EveCaptcha (Windows)** (https://rb.gy/enmpff)
+        `);
+    }
 });
-
+// More bot download links can be added. I will update this when I get to formatting this text to embed to make the message look much cleaner.
 bot.login(token);
 
 bot.on('ready',() =>{
-    console.log('Your first bot is online!')});   
+    console.log('Your first bot is online!')});   //This message can be changed to anything you choose. It will show in the terminal when the bot is enabled.
